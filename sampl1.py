239798ed9033afcb85d959202c314916f7a1c16f
aksa <aksaakku8@gmail.com>
2023-06-20 16:32:06 +0530

    Initial commit

---
+++ sampl1.py
@@ -0,0 +1,19 @@
+eng = 30
+mal = 10
+chem = 15
+phy = 33 
+maths = 20
+sum=(eng+mal+chem+phy+maths)/5
+print(sum) 
+if (sum <=100 and sum>=90):
+    print('A') 
+elif (sum <= 90 and sum >=80):
+    print('B')
+elif (sum <= 80 and sum >=70):
+    print('c')
+elif (sum <= 70 and sum >=60):
+    print('d') 
+elif (sum <=50):
+    print('FAILED') 
+      
+    
